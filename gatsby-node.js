0ed404cdb8aa6ec7d500215a3a9e49085f409d83
tifus69 <tifus69@hotmail.com>
2020-03-28 18:40:34 +0100

    :recycle: add additional datas to context

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,7 +13,33 @@ exports.createPages = async function({ actions, graphql }) {
     actions.createPage({
       path: slug,
       component: require.resolve(`./src/templates/table.js`),
-      context: { slug: slug },
+      context: {
+        slug: slug,
+        additionalData: {
+          descriptions: [
+            {
+              id: 0,
+              value: "The basics for private and secure communications",
+            },
+            { id: 1, value: "Fully featured mailbox with advanced protection" },
+            { id: 2, value: "ProtonMail for professionals and businesses" },
+            { id: 3, value: "Protection for families and small businesses" },
+          ],
+          features: [
+            {
+              id: 1,
+              value:
+                "Supports folders, labels, filters auto-reply, IMAP, SMTP and more",
+            },
+            {
+              id: 2,
+              value:
+                "Catch all emails, multi users management, priority support and more",
+            },
+            { id: 3, value: "Priority support" },
+          ],
+        },
+      },
     })
   })
 }
